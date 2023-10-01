53a9860fdf5fcd4fb2f9d2a7da3191a9ab9394ae
Delziron Braz <44593154+DelzironBraz@users.noreply.github.com>
2023-10-01 17:49:24 -0300

    feat: create listing section (#4)

---
+++ src/interface/plants.ts
@@ -0,0 +1,5 @@
+export interface Plants {
+    id: number,
+    name: string,
+    img: string
+}
---
+++ src/utils/plants.ts
@@ -0,0 +1,12 @@
+import houseplant2 from '../assets/houseplant2.png'
+import houseplant3 from '../assets/houseplant3.png'
+import houseplant4 from '../assets/houseplant4.png'
+import houseplant5 from '../assets/houseplant5.png'
+import { Plants } from '../interface/plants'
+
+export const plants: Plants[] = [
+    { id: 0, name: 'Annual Vince', img: houseplant2 },
+    { id: 1, name: 'Coffee Plant', img: houseplant3 },
+    { id: 2, name: 'Button Fern', img: houseplant4 },
+    { id: 3, name: 'Spider Plant', img: houseplant5 },
+]
