196099c284c8e53bd3f278c16a5a1ae2f0e08e40
Delziron Braz <delziron.braz@hotmail.com>
2023-10-01 18:39:51 -0300

    feat: create activity area

--- src/components/body/Body.tsx
+++ src/components/body/Body.tsx
@@ -1,14 +1,16 @@
+import Actividy from '../actividy/Activity';
 import Listing from '../listing/Listing';
 import Top from '../top/Top';
 import './Body.scss';
 
 const Body = () => {
-  return(
+  return (
     <>
       <div className="main">
         <Top />
-        <div className="main-footer">
-            <Listing />
+        <div className="main-content">
+          <Listing />
+          <Actividy />
         </div>
       </div>
     </>
