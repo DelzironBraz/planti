41b9041cadd5894eb93ae6796d3389accf9f92ea
Delziron Braz <delziron.braz@hotmail.com>
2023-10-18 18:23:29 -0300

    refactor: create collapse sidebar

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -7,106 +7,115 @@ import { MdDeliveryDining, MdOutlineExplore, MdOutlinePermContactCalendar } from
 import { BsTrophy } from 'react-icons/bs';
 import { AiOutlinePieChart, AiOutlineCreditCard, AiOutlineQuestionCircle } from 'react-icons/ai';
 import { BiTrendingUp } from 'react-icons/bi';
+import { useState } from 'react';
 
 const Sidebar = () => {
-    return(
-      <>
-        <div className="sidebar">
-          <div className="logo">
-            <ImLeaf />
-            <h2>Planti.</h2>
-          </div>
+  const [isCollapse, setIsCollapse] = useState(true);
 
-          <div className="menu">
-            <h3 className="title">Quick menu</h3>
-            <ul className="menu-list">
-              <li className="menu-list-item">
-                <a href="#" className="menu-list-item-link">
-                  <IoMdSpeedometer className="icon" />
-                  <span className="menu-list-item-text">
-                    Dashboard
-                  </span>
-                </a>
-              </li>
-              <li className="menu-list-item">
-                <a href="#" className="menu-list-item-link">
-                  <MdDeliveryDining className="icon" />
-                  <span className="menu-list-item-text">
-                    My Orders
-                  </span>
-                </a>
-              </li>
-              <li className="menu-list-item">
-                <a href="#" className="menu-list-item-link">
-                  <MdOutlineExplore className="icon" />
-                  <span className="menu-list-item-text">
-                    Explore
-                  </span>
-                </a>
-              </li>
-              <li className="menu-list-item">
-                <a href="#" className="menu-list-item-link">
-                  <BsTrophy className="icon" />
-                  <span className="menu-list-item-text">
-                    Products
-                  </span>
-                </a>
-              </li>
-            </ul>
-          </div>
+  const handleIsCollapse = () => {
+    setIsCollapse(!isCollapse)
+  }
 
-          <div className="settings">
-            <h3 className="title">Settings</h3>
-            <ul className="settings-list">
-              <li className="settings-list-item"> 
-                <a href="#" className="settings-list-item-link">
-                  <AiOutlinePieChart className="icon" />
-                  <span className="settings-list-item-text">
-                    Charts
-                  </span>
-                </a>
-              </li>
-              <li className="settings-list-item">
-                <a href="#" className="settings-list-item-link">
-                  <BiTrendingUp className="icon" />
-                  <span className="settings-list-item-text">
-                    Trends
-                  </span>
-                </a>
-              </li>
-              <li className="settings-list-item">
-                <a href="#" className="settings-list-item-link">
-                  <MdOutlinePermContactCalendar className="icon" />
-                  <span className="settings-list-item-text">
-                    Contact
-                  </span>
-                </a>
-              </li>
-              <li className="settings-list-item">
-                <a href="#" className="settings-list-item-link">
-                  <AiOutlineCreditCard className="icon" />
-                  <span className="settings-list-item-text">
-                    Billing
-                  </span>
-                </a>
-              </li>
-            </ul>
-          </div>
+  const sidebarClasses = `sidebar ${isCollapse ? 'collapse' : ''}`;
 
-          <div className="sidebar-card">
-            <AiOutlineQuestionCircle className="icon" />
-            <div className="sidebar-card-content">
-              <div className="circle1"></div>
-              <div className="circle2"></div>
+  return (
+    <>
+      <div className={sidebarClasses}>
+        <button className="logo" onClick={handleIsCollapse}>
+          <ImLeaf className="leaf" />
+          <h2>Planti.</h2>
+        </button>
 
-              <h3>Help Center</h3>
-              <p>Having trouble in Planti, please contact us from for more questions</p>
-              <button className="btn">Go to Help Center</button>
-            </div>
+        <div className="menu">
+          <h3 className="title">Quick menu</h3>
+          <ul className="menu-list">
+            <li className="menu-list-item">
+              <a href="#" className="menu-list-item-link">
+                <IoMdSpeedometer className="icon" />
+                <span className="menu-list-item-text">
+                  Dashboard
+                </span>
+              </a>
+            </li>
+            <li className="menu-list-item">
+              <a href="#" className="menu-list-item-link">
+                <MdDeliveryDining className="icon" />
+                <span className="menu-list-item-text">
+                  My Orders
+                </span>
+              </a>
+            </li>
+            <li className="menu-list-item">
+              <a href="#" className="menu-list-item-link">
+                <MdOutlineExplore className="icon" />
+                <span className="menu-list-item-text">
+                  Explore
+                </span>
+              </a>
+            </li>
+            <li className="menu-list-item">
+              <a href="#" className="menu-list-item-link">
+                <BsTrophy className="icon" />
+                <span className="menu-list-item-text">
+                  Products
+                </span>
+              </a>
+            </li>
+          </ul>
+        </div>
+
+        <div className="settings">
+          <h3 className="title">Settings</h3>
+          <ul className="settings-list">
+            <li className="settings-list-item">
+              <a href="#" className="settings-list-item-link">
+                <AiOutlinePieChart className="icon" />
+                <span className="settings-list-item-text">
+                  Charts
+                </span>
+              </a>
+            </li>
+            <li className="settings-list-item">
+              <a href="#" className="settings-list-item-link">
+                <BiTrendingUp className="icon" />
+                <span className="settings-list-item-text">
+                  Trends
+                </span>
+              </a>
+            </li>
+            <li className="settings-list-item">
+              <a href="#" className="settings-list-item-link">
+                <MdOutlinePermContactCalendar className="icon" />
+                <span className="settings-list-item-text">
+                  Contact
+                </span>
+              </a>
+            </li>
+            <li className="settings-list-item">
+              <a href="#" className="settings-list-item-link">
+                <AiOutlineCreditCard className="icon" />
+                <span className="settings-list-item-text">
+                  Billing
+                </span>
+              </a>
+            </li>
+          </ul>
+        </div>
+
+        <div className="card">
+          <AiOutlineQuestionCircle className="icon" />
+          <div className="card-content">
+            <div className="circle1"></div>
+            <div className="circle2"></div>
+
+            <h3>Help Center</h3>
+            <p>Having trouble in Planti, please contact us from for more questions</p>
+            <button className="btn">Go to Help Center</button>
           </div>
         </div>
-      </>
-    );
-  }
-  
-  export default Sidebar
+      </div>
+    </>
+  );
+}
+
+export default Sidebar
